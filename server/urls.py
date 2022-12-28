932ee8e050c338a942eaac3ea89fff9d0af8fc76
Zehan Khan <zehan9211@gmail.com>
2022-12-28 23:21:54 +0530

    migrate database, create admin panel and model created

--- server/urls.py
+++ server/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from server import views
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index)
+    path('',views.index, name='index'),
+    path('about/', views.about, name="about")
 ]
--- server/views.py
+++ server/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index( request ):
     return HttpResponse("Hello from Backend");
+
+def about( request ):
+    # return HttpResponse("welcome to about")
+    return render(request,'files/about.html',{'data':"test-data"})
