2d8562807fa50cd6d8e75be2608e731ce95076a9
Zehan Khan <zehan9211@gmail.com>
2022-12-29 00:28:48 +0530

    route for get for all list  and post  create

--- server/urls.py
+++ server/urls.py
@@ -20,5 +20,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index, name='index'),
-    path('about/', views.about, name="about")
+    path('about/', views.about, name="about"),
+    path('books/', views.book)
 ]
--- server/views.py
+++ server/views.py
@@ -1,10 +1,40 @@
 from django.http import HttpResponse
+from django.http import JsonResponse
 from django.shortcuts import render
+from rest_framework.response import Response
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.decorators import api_view
+from rest_framework import status
 
 
-def index( request ):
-    return HttpResponse("Hello from Backend");
+@api_view(['GET', 'POST'])
+def book(request):
 
-def about( request ):
+    if request.method == 'GET':
+        # get all the book
+        # serialize them
+        # return json
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    if request.method == 'POST':
+        # create a book
+        # validate the data
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def index(request):
+    return HttpResponse("Hello from Backend")
+
+
+def about(request):
     # return HttpResponse("welcome to about")
-    return render(request,'files/about.html',{'data':"test-data"})
+    return render(request, 'files/about.html', {'data': "test-data"})
